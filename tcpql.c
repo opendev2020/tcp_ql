38d5bbea7e3d4f3999f4babd9b531d4d22d78ba8
derekwin <1446103183@qq.com>
2022-06-27 02:39:03 +0200

    update

--- tcpql.c
+++ tcpql.c
@@ -8,7 +8,7 @@
 #define	state1_max	19	// throughput diff value
 #define	state2_max	19	// rtt diff value
 
-#define	TCP_QL_SCALE	1024
+#define	Q_CONG_SCALE	1024
 
 #define	numOfAction	4
 
@@ -37,7 +37,7 @@ enum action{
     CWND_NOTHING,
 };
 
-enum tcp_ql_mode{
+enum q_cong_mode{
 	NOTHING,
 	TRAINING,
 	ESTIMATE_MIN_RTT,
@@ -53,7 +53,7 @@ typedef struct{
 
 static Matrix matrix;
 
-struct Tcp_ql{
+struct Q_cong{
 	u64	alpha;
 	bool	forced_update;
 	
@@ -132,17 +132,17 @@ static int getMatValue(Matrix *m, u8 row1, u8 row2, u8 row3,  u8 col){
 	return *(m -> mat + index);
 }
 
-static u32 tcp_ql_ssthresh(struct sock *sk){
+static u32 q_cong_ssthresh(struct sock *sk){
 	return TCP_INFINITE_SSTHRESH; /* TCP Q-congestion does not use ssthresh */
 }
 
 static void reset_cwnd(struct sock *sk, const struct rate_sample *rs){
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 	struct tcp_sock *tp = tcp_sk(sk);
 
-	if (tql -> mode == STARTUP){
+	if (qc -> mode == STARTUP){
 		if(inet_csk(sk) -> icsk_ca_state >= TCP_CA_Recovery){
-			tql -> mode = NOTHING; 
+			qc -> mode = NOTHING; 
 		}
 		else{
 			tp -> snd_cwnd += rs -> acked_sacked;
@@ -184,7 +184,7 @@ int softsigntt(int value, int smooth_throughput){	// softsign for throughput rel
 
 
 static u32 getAction(struct sock *sk, const struct rate_sample *rs){
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 
 	u32 Q[numOfAction];
 	u8 i;
@@ -194,7 +194,7 @@ static u32 getAction(struct sock *sk, const struct rate_sample *rs){
 	u32 rand;	
 
 	for(i=0; i<numOfAction; i++){
-		Q[i] = getMatValue(&matrix, tql -> current_state[0], tql->current_state[1], tql->current_state[2],i);
+		Q[i] = getMatValue(&matrix, qc -> current_state[0], qc->current_state[1], qc->current_state[2],i);
 	}
 
 	max_tmp = Q[0];
@@ -215,29 +215,25 @@ static u32 getAction(struct sock *sk, const struct rate_sample *rs){
 		max_index = (rand%numOfAction);
 	}
 
-	if(max_index == CWND_DOWN){
-		return max_index;
-	}
-
 	return epsilon_expore(max_index);
 }
 
 static int getRewardFromEnvironment(struct sock *sk, const struct rate_sample *rs){
 	//struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 	u32 retransmit_division_factor; 
     int diff_throughput;
     int diff_delay;
     int smooth_divide_current_throughput;
     int result;
 
-	retransmit_division_factor = tql -> retransmit_during_interval + 1;
+	retransmit_division_factor = qc -> retransmit_during_interval + 1;
 	if(retransmit_division_factor == 0 || rs->rtt_us == 0)
 		return 0;
 	
-	diff_throughput = softsignt((int)(tql -> estimated_throughput - tql -> smooth_throughput));
-	diff_delay = softsignr((int)((rs -> rtt_us - tql -> min_rtt_us)-(tql -> pre_rtt - tql -> min_rtt_us)));	// measurement inaccuracy
-	smooth_divide_current_throughput =  (int)(tql -> smooth_throughput / (tql -> estimated_throughput==0?1:tql -> estimated_throughput)) > 20 ? 20 : (int)(tql -> smooth_throughput / (tql -> estimated_throughput==0?1:tql -> estimated_throughput));
+	diff_throughput = softsignt((int)(qc -> estimated_throughput - qc -> smooth_throughput));
+	diff_delay = softsignr((int)((rs -> rtt_us - qc -> min_rtt_us)-(qc -> pre_rtt - qc -> min_rtt_us)));	// measurement inaccuracy
+	smooth_divide_current_throughput =  (int)(qc -> smooth_throughput / (qc -> estimated_throughput==0?1:qc -> estimated_throughput)) > 20 ? 20 : (int)(qc -> smooth_throughput / (qc -> estimated_throughput==0?1:qc -> estimated_throughput));
 
     /* 
 	 * Utility Function
@@ -255,9 +251,9 @@ static int getRewardFromEnvironment(struct sock *sk, const struct rate_sample *r
 
 static void executeAction(struct sock *sk, const struct rate_sample *rs){
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 
-	switch(tql -> action){
+	switch(qc -> action){
 		case CWND_UP_30:
 			tp -> snd_cwnd  = tp->snd_cwnd + 30/ (tp->snd_cwnd);
 			break;
@@ -275,7 +271,7 @@ static void executeAction(struct sock *sk, const struct rate_sample *rs){
 	}
 }
 
-static u32 tcp_ql_undo_cwnd(struct sock* sk){
+static u32 q_cong_undo_cwnd(struct sock* sk){
 	struct tcp_sock *tp = tcp_sk(sk);
 	
 	// printk(KERN_INFO "undo congestion control");
@@ -284,42 +280,42 @@ static u32 tcp_ql_undo_cwnd(struct sock* sk){
 
 static void calc_retransmit_during_interval(struct sock* sk){
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 
-	tql -> retransmit_during_interval = (tp -> total_retrans - tql -> last_packet_loss) * training_interval_msec / jiffies_to_msecs(tcp_jiffies32 - tql -> last_update_stamp);
-	tql -> last_packet_loss = tp -> total_retrans; 
+	qc -> retransmit_during_interval = (tp -> total_retrans - qc -> last_packet_loss) * training_interval_msec / jiffies_to_msecs(tcp_jiffies32 - qc -> last_update_stamp);
+	qc -> last_packet_loss = tp -> total_retrans; 
 }
 
 static void calc_throughput(struct sock *sk){
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 
 	u32 segout_for_interval;
 	
-	segout_for_interval = (tp -> segs_out - tql ->last_sequence) * tp ->mss_cache; 
+	segout_for_interval = (tp -> segs_out - qc ->last_sequence) * tp ->mss_cache; 
 
-	tql -> estimated_throughput = segout_for_interval * 8 / jiffies_to_msecs(tcp_jiffies32 - tql -> last_update_stamp); 
-	tql -> smooth_throughput = ((7 * tql -> smooth_throughput)>>3) + ((tql -> estimated_throughput)>>3);		// 1/8
-	tql -> last_sequence = tp -> segs_out;
+	qc -> estimated_throughput = segout_for_interval * 8 / jiffies_to_msecs(tcp_jiffies32 - qc -> last_update_stamp); 
+	qc -> smooth_throughput = ((7 * qc -> smooth_throughput)>>3) + ((qc -> estimated_throughput)>>3);		// 1/8
+	qc -> last_sequence = tp -> segs_out;
 }
 
 static int update_state(struct sock *sk, const struct rate_sample *rs){
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
     int current_rtt;
 	u8 i; 
 
 	for (i=0; i<numOfState; i++)
-		tql -> prev_state[i] = tql -> current_state[i];
+		qc -> prev_state[i] = qc -> current_state[i];
 	
-	tql -> current_state[0] = softsigntt((int)tql -> estimated_throughput, (int)tql -> smooth_throughput);
-	tql -> current_state[1] = softsign((int)(tql -> estimated_throughput - tql -> smooth_throughput));
+	qc -> current_state[0] = softsigntt((int)qc -> estimated_throughput, (int)qc -> smooth_throughput);
+	qc -> current_state[1] = softsign((int)(qc -> estimated_throughput - qc -> smooth_throughput));
 	current_rtt = rs->rtt_us;
-	tql -> current_state[2] = softsign((int)(current_rtt - tql-> pre_rtt));		// pre_rtt是比smoothrtt好的，但是这里的问题是一秒一取造成了pre很不准确
+	qc -> current_state[2] = softsign((int)(current_rtt - qc-> pre_rtt));		// pre_rtt是比smoothrtt好的，但是这里的问题是一秒一取造成了pre很不准确
 	return current_rtt;
 }
 
 static void update_Qtable(struct sock *sk, const struct rate_sample *rs){
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 
 	u32 thisQ[numOfAction]; 
 	u32 newQ[numOfAction];
@@ -328,8 +324,8 @@ static void update_Qtable(struct sock *sk, const struct rate_sample *rs){
 	int max_tmp; 
 	
 	for(i=0; i<numOfAction; i++){
-		thisQ[i] = getMatValue(&matrix, tql->prev_state[0], tql->prev_state[1], tql->prev_state[2], i);
-		newQ[i] = getMatValue(&matrix, tql->current_state[0], tql->current_state[1], tql->current_state[2], i);
+		thisQ[i] = getMatValue(&matrix, qc->prev_state[0], qc->prev_state[1], qc->prev_state[2], i);
+		newQ[i] = getMatValue(&matrix, qc->current_state[0], qc->current_state[1], qc->current_state[2], i);
 	}
 
 	max_tmp = newQ[0];
@@ -338,155 +334,155 @@ static void update_Qtable(struct sock *sk, const struct rate_sample *rs){
 			max_tmp = newQ[i]; 
 	}
 
-	updated_Qvalue = ((TCP_QL_SCALE-learning_rate)*thisQ[tql ->action] +
+	updated_Qvalue = ((Q_CONG_SCALE-learning_rate)*thisQ[qc ->action] +
 			(learning_rate * (getRewardFromEnvironment(sk,rs) + ((discount_factor * max_tmp)>>4))))>>10;
 
 	if(updated_Qvalue == 0){
-		tql -> exited = 1; 
+		qc -> exited = 1; 
 		return;
 	}
 	
-	setMatValue(&matrix, tql->prev_state[0], tql->prev_state[1], tql->prev_state[2], tql->action, updated_Qvalue);
+	setMatValue(&matrix, qc->prev_state[0], qc->prev_state[1], qc->prev_state[2], qc->action, updated_Qvalue);
 }
 
 static void training(struct sock *sk, const struct rate_sample *rs){
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 	
-	u32 training_timer_expired = after(tcp_jiffies32, tql -> last_update_stamp + msecs_to_jiffies(training_interval_msec)); 
+	u32 training_timer_expired = after(tcp_jiffies32, qc -> last_update_stamp + msecs_to_jiffies(training_interval_msec)); 
 
-	if(training_timer_expired && tql -> mode == NOTHING){
+	if(training_timer_expired && qc -> mode == NOTHING){
 
-		if (tql -> action == 0xffffffff)
+		if (qc -> action == 0xffffffff)
 			goto execute;
 
 		calc_throughput(sk);
 		calc_retransmit_during_interval(sk);
 
-		if (tql -> exited == 1){
+		if (qc -> exited == 1){
 			tp -> snd_cwnd = TCP_INIT_CWND; 
-			tql -> exited = 0; 
+			qc -> exited = 0; 
 			return; 
 		}
 
 		update_Qtable(sk,rs);
 execute:
-		printk(KERN_INFO "execute Action: %u", tql -> action);
-		tql -> action = getAction(sk,rs);
+		printk(KERN_INFO "execute Action: %u", qc -> action);
+		qc -> action = getAction(sk,rs);
 		executeAction(sk, rs);
-		tql -> last_update_stamp = tcp_jiffies32; 
+		qc -> last_update_stamp = tcp_jiffies32; 
 	}
 }
 
 static void update_min_rtt(struct sock *sk, const struct rate_sample* rs){
 	struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
 	u32 estimate_rtt_expired; 
 
 	u32 update_filter_expired = after(tcp_jiffies32, 
-			tql -> last_probertt_stamp + msecs_to_jiffies(probertt_interval_msec));
+			qc -> last_probertt_stamp + msecs_to_jiffies(probertt_interval_msec));
 
 	if (rs -> rtt_us > 0){	
-		if (rs -> rtt_us < tql -> min_rtt_us){
-			tql -> min_rtt_us = rs -> rtt_us;
-			tql -> last_probertt_stamp = tcp_jiffies32; 
-			if (tql -> min_rtt_us < tql-> prop_rtt_us)
-				tql -> prop_rtt_us = tql -> min_rtt_us; 
+		if (rs -> rtt_us < qc -> min_rtt_us){
+			qc -> min_rtt_us = rs -> rtt_us;
+			qc -> last_probertt_stamp = tcp_jiffies32; 
+			if (qc -> min_rtt_us < qc-> prop_rtt_us)
+				qc -> prop_rtt_us = qc -> min_rtt_us; 
 		}
 	}
 
-	if(update_filter_expired && tql -> mode == NOTHING){ 
-		tql -> mode = ESTIMATE_MIN_RTT; 
-		tql -> last_probertt_stamp = tcp_jiffies32; 
-		tql -> prior_cwnd = tp -> snd_cwnd;
+	if(update_filter_expired && qc -> mode == NOTHING){ 
+		qc -> mode = ESTIMATE_MIN_RTT; 
+		qc -> last_probertt_stamp = tcp_jiffies32; 
+		qc -> prior_cwnd = tp -> snd_cwnd;
 		tp -> snd_cwnd = min(tp -> snd_cwnd, estimate_min_rtt_cwnd);
-		tql -> min_rtt_us = rs -> rtt_us;
+		qc -> min_rtt_us = rs -> rtt_us;
 	}
 
-	if(tql -> mode == ESTIMATE_MIN_RTT){
+	if(qc -> mode == ESTIMATE_MIN_RTT){
 		estimate_rtt_expired = after(tcp_jiffies32, 
-				tql -> last_probertt_stamp + msecs_to_jiffies(max_probertt_duration_msecs)); 
+				qc -> last_probertt_stamp + msecs_to_jiffies(max_probertt_duration_msecs)); 
 		if(estimate_rtt_expired){
-			tql -> mode = NOTHING; 
-			tp -> snd_cwnd = tql -> prior_cwnd;
+			qc -> mode = NOTHING; 
+			tp -> snd_cwnd = qc -> prior_cwnd;
 		}
 	}
 }
 
-static void tcp_ql_main(struct sock *sk, const struct rate_sample *rs){
+static void q_cong_main(struct sock *sk, const struct rate_sample *rs){
 	// struct tcp_sock *tp = tcp_sk(sk);
-	struct Tcp_ql *tql = inet_csk_ca(sk);
+	struct Q_cong *qc = inet_csk_ca(sk);
     int current_rtt;
 
 	reset_cwnd(sk, rs);
 	current_rtt = update_state(sk,rs);
 	training(sk, rs);
-	tql -> pre_rtt = current_rtt;
+	qc -> pre_rtt = current_rtt;
 	update_min_rtt(sk,rs);
 }
 
 
 
-static void init_Tcp_ql(struct sock *sk){
-	struct Tcp_ql *tql;
+static void init_Q_cong(struct sock *sk){
+	struct Q_cong *qc;
 	struct tcp_sock *tp = tcp_sk(sk);
-	u8 Qtable_row[numOfState] = {state0_max, state1_max, state2_max};
-	u8 Qtable_col = numOfAction; 
+	u8 Q_row[numOfState] = {state0_max, state1_max, state2_max};
+	u8 Q_col = numOfAction; 
 
-	tql = inet_csk_ca(sk);
+	qc = inet_csk_ca(sk);
 
-	tql -> mode = STARTUP;
-	tql -> last_sequence = 0;
-	tql -> estimated_throughput = 0;
-	tql -> smooth_throughput = 0;
-	tql -> last_update_stamp = tcp_jiffies32;
-	tql -> last_packet_loss = 0;
+	qc -> mode = STARTUP;
+	qc -> last_sequence = 0;
+	qc -> estimated_throughput = 0;
+	qc -> smooth_throughput = 0;
+	qc -> last_update_stamp = tcp_jiffies32;
+	qc -> last_packet_loss = 0;
 
-	tql -> last_probertt_stamp = tcp_jiffies32;
-	tql -> min_rtt_us = tcp_min_rtt(tp);
-	tql -> prop_rtt_us = tcp_min_rtt(tp);
-	tql -> pre_rtt = tcp_min_rtt(tp);
-	tql -> prior_cwnd = 0;
-	tql -> retransmit_during_interval = 0;
+	qc -> last_probertt_stamp = tcp_jiffies32;
+	qc -> min_rtt_us = tcp_min_rtt(tp);
+	qc -> prop_rtt_us = tcp_min_rtt(tp);
+	qc -> pre_rtt = tcp_min_rtt(tp);
+	qc -> prior_cwnd = 0;
+	qc -> retransmit_during_interval = 0;
 
-	tql -> action = -1; 
-	tql -> exited = 0; 
-	tql -> prev_state[0] = 0;
-	tql -> prev_state[1] = 0; 
-	tql -> prev_state[2] = 0;
-	tql -> current_state[0] = 0;
-	tql -> current_state[1] = 0;
-	tql -> current_state[2] = 0;
+	qc -> action = -1; 
+	qc -> exited = 0; 
+	qc -> prev_state[0] = 0;
+	qc -> prev_state[1] = 0; 
+	qc -> prev_state[2] = 0;
+	qc -> current_state[0] = 0;
+	qc -> current_state[1] = 0;
+	qc -> current_state[2] = 0;
 
-	createMatrix(&matrix, Qtable_row, Qtable_col);
+	createMatrix(&matrix, Q_row, Q_col);
 }
 
-static void release_Tcp_ql(struct sock* sk){
+static void release_Q_cong(struct sock* sk){
 	eraseMatrix(&matrix);
 }
 
-struct tcp_congestion_ops tcp_ql = {
+struct tcp_congestion_ops q_cong = {
 	.flags		= TCP_CONG_NON_RESTRICTED, 
-	.init		= init_Tcp_ql,
-	.release	= release_Tcp_ql,
+	.init		= init_Q_cong,
+	.release	= release_Q_cong,
 	.name 		= "tcpql",
 	.owner		= THIS_MODULE,
-	.ssthresh	= tcp_ql_ssthresh,
-	.cong_control	= tcp_ql_main,
-	.undo_cwnd 	= tcp_ql_undo_cwnd,
+	.ssthresh	= q_cong_ssthresh,
+	.cong_control	= q_cong_main,
+	.undo_cwnd 	= q_cong_undo_cwnd,
 };
 
-static int __init Tcp_ql_init(void){
-	BUILD_BUG_ON(sizeof(struct Tcp_ql) > ICSK_CA_PRIV_SIZE);
-	return tcp_register_congestion_control(&tcp_ql);
+static int __init Q_cong_init(void){
+	BUILD_BUG_ON(sizeof(struct Q_cong) > ICSK_CA_PRIV_SIZE);
+	return tcp_register_congestion_control(&q_cong);
 }
 
-static void __exit Tcp_ql_exit(void){
-	tcp_unregister_congestion_control(&tcp_ql);
+static void __exit Q_cong_exit(void){
+	tcp_unregister_congestion_control(&q_cong);
 }
 
-module_init(Tcp_ql_init);
-module_exit(Tcp_ql_exit);
+module_init(Q_cong_init);
+module_exit(Q_cong_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("HuiQi,JinyaoLiu");
